3adf4871332918cd9ecf4d78130aa21a10fb5961
diegomoreno <diego.moreno@linx.com.br>
2020-01-13 07:35:25 -0300

    Updates

---
+++ precache-manifest.50493386713a6efd0437760d684dcc42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be69607d57b87de93c9a7114d2f51835",
+    "url": "/FacebookAdsInterest/index.html"
+  },
+  {
+    "revision": "985e145a0a6d4ff3bac8",
+    "url": "/FacebookAdsInterest/static/css/main.1ef281c7.chunk.css"
+  },
+  {
+    "revision": "b31a794490078a9bee7a",
+    "url": "/FacebookAdsInterest/static/js/2.820c5a69.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/FacebookAdsInterest/static/js/2.820c5a69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "985e145a0a6d4ff3bac8",
+    "url": "/FacebookAdsInterest/static/js/main.2c1bb741.chunk.js"
+  },
+  {
+    "revision": "a3902442250e800e2d0f",
+    "url": "/FacebookAdsInterest/static/js/runtime-main.520370be.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/facebookinterests/precache-manifest.60c8ba94dcf46435c9628a576ef6944a.js"
+  "/FacebookAdsInterest/precache-manifest.50493386713a6efd0437760d684dcc42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/facebookinterests/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FacebookAdsInterest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
